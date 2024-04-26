6f2f05e452a6e4bee34e958ddbec75c33d31b444
AmziJeffs <amzijeffs0@gmail.com>
2024-04-25 22:17:26 -0400

    Finish participant parsing utility function

--- utilities.py
+++ utilities.py
@@ -6,26 +6,47 @@
 # Charged parties follow 'Charged Party / Respondent,'
 # Charging parties follow 'Charging Party,'
 # Involved parties follow 'Involved Party,' 
-# Returns a list [charged, charging, involved] where each entry is a list 
-# of parties, which are strings. Lists can be empty.
-# TODO: trim leading spaces and line breaks 
-def get_charged_and_charging(S):
-	charged = []
-	chargers = []
+# Returns a list [charged_parties, charging_parties, involved_parties] 
+# where each entry is a (possibly empty) list of parties, which are strings.
+def get_charged_charging_and_involved(participants):
+	if not isinstance(participants, str):
+		return [[],[],[]]
+    
+	charged_parties = []
+	charging_parties = []
+	involved_parties = []
 
 	# split by charged
-	S = S.split("Charged Party / Respondent,")
-	for s in S:
-		if len(s) > 0:
-			if "Charging Party, " in s:
-				SS = s.split("Charging Party,")
-				if len(SS[0]) > 0:
-					charged += [SS[0]]
-				chargers += SS[1:]
+	split_on_charged = participants.split("Charged Party / Respondent,")
+	for S in split_on_charged:
+		if len(S) > 0:
+			if "Charging Party," in S:
+				split_on_charging = S.split("Charging Party,")
+				if len(split_on_charging[0]) > 0:
+					if "Involved Party," in split_on_charging[0]:
+						split_on_involved = split_on_charging[0].split("Involved Party,")
+						if len(split_on_involved[0]) > 0:
+							charged_parties += [split_on_involved[0].strip()]
+						involved_parties += [p.strip() for p in split_on_involved[1:]]
+					else:
+						charged_parties += [split_on_charging[0].strip()]
+				for SS in split_on_charging[1:]:
+					if "Involved Party," in SS:
+						split_on_involved = SS.split("Involved Party,")
+						if len(split_on_involved[0]) > 0:
+							charging_parties += [split_on_involved[0].strip()]
+						involved_parties += [p.strip() for p in split_on_involved[1:]]
+					else:
+						charging_parties += [SS.strip()]
+			elif "Involved Party," in S:
+				split_on_involved = S.split("Involved Party,")
+				if len(split_on_involved[0]) > 0:
+					charged_parties += [split_on_involved[0].strip()]
+				involved_parties += [p.strip() for p in split_on_involved[1:]]
 			else:
-				charged += [s]
+				charged_parties+= [S.strip()]
 
-	return [charged, chargers]
+	return [charged_parties, charging_parties, involved_parties]
 
 
 # Returns a list of unions present in a given string. Good for parsing case
